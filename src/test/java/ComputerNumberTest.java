8b8b7f89f23bb802fd2bbb61f6e9172d8e736152
NamYoung Kim <095201a@naver.com>
2022-04-17 15:49:30 +0900

    1. 그레이들 assertj 의존성 추가 2. ComputerNumberTest.java 추가

---
+++ src/test/java/ComputerNumberTest.java
@@ -0,0 +1,23 @@
+import Model.ComputerNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ComputerNumberTest {
+
+    private final List<Integer> rspNumbers = Arrays.asList(1,2,3);
+
+    @Test
+    @DisplayName("컴퓨터 자동 번호가 잘 생성되었니?")
+    void computerNumber(){
+        ComputerNumber computerNumber = new ComputerNumber();
+        Integer number = computerNumber.getComputerNumber();
+
+        assertThat(rspNumbers).contains(number);
+    }
+}
+
