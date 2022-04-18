2132af878dd9e8842f5799b4c5c352cddf09e2cf
NamYoung Kim <095201a@naver.com>
2022-04-18 15:52:41 +0900

    게임 결과에 따른 카운트 테스트

---
+++ src/test/java/GameCountTest.java
@@ -0,0 +1,29 @@
+import Enum.*;
+import Exception.RspException;
+import Model.WinDrawCount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class GameCountTest {
+
+    @Test
+    @DisplayName("게임 이겼을 시 카운트 개수")
+    void winCount(){
+        Result.gameCount(Result.WIN);
+        int winCount = WinDrawCount.getWinCount();
+
+        assertThat(winCount == 1).isTrue();
+    }
+
+    @Test
+    @DisplayName("게임 비겼을 시 카운트 개수")
+    void drawCount(){
+        Result.gameCount(Result.DRAW);
+        int drawCount = WinDrawCount.getDrawCount();
+
+        assertThat(drawCount == 1).isTrue();
+    }
+
+}
