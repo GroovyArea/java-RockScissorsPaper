8ab8dd109d64a6fbb60eaa0963e570a92cea58ac
NamYoung Kim <095201a@naver.com>
2022-04-18 15:33:07 +0900

    입력 예외 테스트 코드 추가

--- src/test/java/RspInputTest.java
+++ src/test/java/RspInputTest.java
@@ -1,9 +1,15 @@
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import Enum.*;
 import Exception.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class RspInputTest {
 
@@ -14,6 +20,16 @@ public class RspInputTest {
 
         Rsp input = Rsp.of(1);
 
-        assertThat(rsp == input);
+        assertThat(rsp == input).isTrue();
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, -2, 199, 349})
+    @DisplayName("잘못 입력했을 경우 예외")
+    void rspInputFail(int value){
+        assertThatExceptionOfType(RspException.class)
+                .isThrownBy(() -> Rsp.of(value))
+                .withMessage("가위바위보 입력이 잘못 되었습니다.");
     }
 }
