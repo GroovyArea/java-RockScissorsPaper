8d22359890eb923edfcadbbfb885c38898b49cc8
NamYoung Kim <095201a@naver.com>
2022-04-19 23:49:46 +0900

    gameCount() 변경 및 예외 메시지 상수 추가

--- src/main/java/Enum/Result.java
+++ src/main/java/Enum/Result.java
@@ -1,36 +1,45 @@
 package Enum;
 
 import Model.WinDrawCount;
+import Exception.GameCountException;
 
 public enum Result {
 
-	WIN, LOSE, DRAW;
+    WIN, LOSE, DRAW;
 
-	// 결과 로직 반환
-	public static Result getGameResult(Rsp player, Rsp com) {
+    private static final String RESULT_EXCEPTION = "진 결과를 받아올 수 없습니다.";
 
-		// 이김
-		if (com == Rsp.ROCK && player == Rsp.PAPER 
-				|| com == Rsp.SCISSORS && player == Rsp.ROCK
-				|| com == Rsp.PAPER && player == Rsp.SCISSORS) {
-			return WIN;
-		}
+    // 결과 로직 반환
+    public static Result getGameResult(Rsp player, Rsp com) {
 
-		// 비김
-		if (com == player) {
-			return DRAW;
-		}
-		
-		// 짐
-		return LOSE;
-	}
-	
-	// enum 타입 결과를 매개변수로 받아 승 무 카운트
-	public static void gameCount(Result result) {
-		if(result == WIN)
-			WinDrawCount.plusWinCount();
-		if(result == DRAW)
-			WinDrawCount.plusDrawCount();
-	}
-	
+        // 이김
+        if (com == Rsp.ROCK && player == Rsp.PAPER
+                || com == Rsp.SCISSORS && player == Rsp.ROCK
+                || com == Rsp.PAPER && player == Rsp.SCISSORS) {
+            return WIN;
+        }
+
+        // 비김
+        if (com == player) {
+            return DRAW;
+        }
+
+        // 짐
+        return LOSE;
+    }
+
+    // enum 타입 결과를 매개변수로 받아 승 무 카운트
+    public static void gameCount(Result result) throws GameCountException {
+
+        switch (result) {
+            case WIN:
+                WinDrawCount.plusWinCount();
+            case DRAW:
+                WinDrawCount.plusDrawCount();
+            default:
+                throw new GameCountException(RESULT_EXCEPTION);
+        }
+
+    }
+
 }
