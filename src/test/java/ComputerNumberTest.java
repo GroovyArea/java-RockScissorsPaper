2c979d3b2c5a71bb4b3da376b14b51ad6be40ef7
NamYoung Kim <095201a@naver.com>
2022-04-19 23:39:04 +0900

    @BeforeEach 에너테이션 추가

--- src/test/java/ComputerNumberTest.java
+++ src/test/java/ComputerNumberTest.java
@@ -1,4 +1,6 @@
 import Model.ComputerNumber;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +11,16 @@ import static org.assertj.core.api.Assertions.*;
 
 public class ComputerNumberTest {
 
-    private final List<Integer> rspNumbers = Arrays.asList(1,2,3);
+    private List<Integer> rspNumbers;
+
+    @BeforeEach
+    void Numbers() {
+       rspNumbers = Arrays.asList(1, 2, 3);
+    }
 
     @Test
     @DisplayName("컴퓨터 자동 번호가 잘 생성되었니?")
-    void computerNumber(){
+    void computerNumber() {
         ComputerNumber computerNumber = new ComputerNumber();
         Integer number = computerNumber.getComputerNumber();
 
