7cb13e7cfa84357423001b853b1fa84dec74062e
NamYoung Kim <095201a@naver.com>
2022-04-22 12:28:58 +0900

    List 이용 수정

--- src/test/java/ComputerNumberTest.java
+++ src/test/java/ComputerNumberTest.java
@@ -1,30 +1,38 @@
 import Model.ComputerNumber;
+import Enum.*;
+import Exception.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class ComputerNumberTest {
 
-    private List<Integer> rspNumbers;
+    static List<Integer> numbers1() {
+        final List<Integer> numbers = new ArrayList<>();
 
-    @BeforeEach
-    void Numbers() {
-       rspNumbers = Arrays.asList(1, 2, 3);
+        for (int i = 0; i < 10; i++) {
+            ComputerNumber computerNumber = new ComputerNumber();
+            numbers.add(computerNumber.getComputerNumber());
+        }
+
+        return numbers;
     }
 
     @Test
-    @DisplayName("컴퓨터 자동 번호가 잘 생성되었니?")
-    void computerNumber() {
-        ComputerNumber computerNumber = new ComputerNumber();
-        Integer number = computerNumber.getComputerNumber();
-
-        assertThat(rspNumbers).contains(number);
+    @DisplayName("컴퓨터 자동 번호들이 범위 안에서 잘 생성되는지?")
+    void test() {
+        assertThat(numbers1()).contains(1,2,3);
     }
 }
 
