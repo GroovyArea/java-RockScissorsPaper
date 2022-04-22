88be2c9ea8b93fdaf05755828fa4a6d6a47f0297
NamYoung Kim <095201a@naver.com>
2022-04-22 12:37:14 +0900

    Stream과 @MethodSource 이용 수정

--- src/test/java/PrintGameResultTest.java
+++ src/test/java/PrintGameResultTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import Enum.*;
 
@@ -21,26 +24,21 @@ public class PrintGameResultTest {
 
     private List<PrintGameResult> resultMessages;
 
-    @BeforeEach
-    void beforeResults() {
-        results = Arrays.asList(Result.WIN, Result.DRAW, Result.LOSE);
+    // 게임에서 진 경우와 이긴 경우의 결과 메세지가 잘 나오는지 살펴보자
+
+    static Stream<Arguments> rspResultsAndPrints(){
+        return Stream.of(
+                Arguments.arguments(Result.WIN, PrintGameResult.WIN_RESULT),
+                Arguments.arguments(Result.LOSE, PrintGameResult.LOSE_RESULT),
+                Arguments.arguments(Result.DRAW, PrintGameResult.DRAW_RESULT)
+        );
     }
 
-    @BeforeEach
-    void beforeMessages() {
-        resultMessages = Arrays.asList(PrintGameResult.WIN_RESULT,PrintGameResult.DRAW_RESULT,PrintGameResult.LOSE_RESULT);
-    }
-
-
-    @Test
+    @ParameterizedTest(name = "게임 결과 : {0}, 출력 메시지 : {1}")
+    @MethodSource("rspResultsAndPrints")
     @DisplayName("게임 결과 메시지가 상황에 맞게 잘 나오니?")
-    void ofTest() {
-        int i = 0;
-        for (Result result : results){
-            assertThat(PrintGameResult.of(result) == resultMessages.get(i)).isTrue();
-            i++;
-        }
-
+    void ofTest(Result result, PrintGameResult printGameResult){
+        assertThat(PrintGameResult.of(result)).isEqualTo(printGameResult);
     }
 
 
