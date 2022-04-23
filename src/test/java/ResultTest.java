30e8cc88bb89c3f63137a10d0052bee22573aaf4
NamYoung Kim <095201a@naver.com>
2022-04-23 13:54:13 +0900

    게임 카운트 테스트 추가

--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -1,13 +1,18 @@
 import Model.Computer;
 import Model.ComputerNumber;
 import Model.Player;
+import Model.WinDrawCount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import Enum.*;
 import Exception.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.CoreMatchers.is;
@@ -32,11 +37,24 @@ public class ResultTest {
         assertThat(result == Result.WIN).isTrue();
     }
 
+    static void gameCountTest(Result result) throws GameCountException {
+        Result.gameCount(result);
+    }
+
+    // 게임 카운트 관련 테스트
+    @Test
+    @DisplayName("단일 게임 win일 경우 win 카운트 증가 여부 판단")
+    void increaseGameCount() throws GameCountException {
+        gameCountTest(Result.WIN);
+        assertThat(WinDrawCount.getWinCount() > 0).isTrue();
+    }
+
     @Test
     @DisplayName("진 결과가 들어갈 경우 예외 발생")
     void validResultTest() {
         assertThatExceptionOfType(GameCountException.class)
                 .isThrownBy(() -> Result.gameCount(Result.LOSE)).withMessage("진 결과를 받아올 수 없습니다.");
     }
+
 
 }
