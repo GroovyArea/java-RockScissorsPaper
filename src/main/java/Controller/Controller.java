41736e16411d1f6fa6e18a950da2458eb1e759e1
NamYoung Kim <095201a@naver.com>
2022-04-24 11:53:37 +0900

    default 문제로 gameCount() 제어문 추가

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -12,49 +12,50 @@ import View.ViewOutput;
 // 요청을 받는 Controller
 public class Controller {
 
-	// 필드 선언
-	private Result result;
-	private Player player;
-	private Computer computer;
+    // 필드 선언
+    private Result result;
+    private Player player;
+    private Computer computer;
 
-	// play 메서드
-	public void playRsp(Rsp playerRsp) throws RspException, GameCountException {
+    // play 메서드
+    public void playRsp(Rsp playerRsp) throws RspException, GameCountException {
 
-		// 참가자 객체 생성
-		player = new Player(playerRsp);
-		// 컴터 객체 생성 및 번호 주입
-		computer = new Computer(new ComputerNumber().getComputerNumber());
-		
-		// 게임결과 반환값을 필드에 저장
-		this.result = gameOf(player, computer);
+        // 참가자 객체 생성
+        player = new Player(playerRsp);
+        // 컴터 객체 생성 및 번호 주입
+        computer = new Computer(new ComputerNumber().getComputerNumber());
 
-		rspPrint();
-		gameCount();
-		changedResultPrint();
-	}
+        // 게임결과 반환값을 필드에 저장
+        this.result = gameOf(player, computer);
 
-	// 승리 무승부 카운트 변환 메서드
-	public void gameCount() throws GameCountException {
-		Result.gameCount(result);
-	}
-	
-	// 가위바위보 상태 값 출력하는 메서드
-	public void rspPrint() throws RspException {
-		ViewOutput.whatYouInput(player.getRsp(), computer.getComRsp());
-	}
-	
-	// viewOutput 결과 바꿔주고 출력하는 메서드
-	public void changedResultPrint() {
-		ViewOutput.singleGameResult(PrintGameResult.of(result));
-	}
-	
-	// 결과 얻어오는 메서드
-	public Result gameOf(Player player, Computer computer) throws RspException {
-		return Result.getGameResult(player.getRsp(), computer.getComRsp());
-	}
+        rspPrint();
+        if (this.result != Result.LOSE) {gameCount();}
+        changedResultPrint();
 
-	// 게임 결과 반환 메서드
-	public Result getGameResult() {
-		return result;
-	}
+    }
+
+    // 승리 무승부 카운트 변환 메서드
+    public void gameCount() throws GameCountException {
+        Result.gameCount(result);
+    }
+
+    // 가위바위보 상태 값 출력하는 메서드
+    public void rspPrint() throws RspException {
+        ViewOutput.whatYouInput(player.getRsp(), computer.getComRsp());
+    }
+
+    // viewOutput 결과 바꿔주고 출력하는 메서드
+    public void changedResultPrint() {
+        ViewOutput.singleGameResult(PrintGameResult.of(result));
+    }
+
+    // 결과 얻어오는 메서드
+    public Result gameOf(Player player, Computer computer) throws RspException {
+        return Result.getGameResult(player.getRsp(), computer.getComRsp());
+    }
+
+    // 게임 결과 반환 메서드
+    public Result getGameResult() {
+        return result;
+    }
 }
