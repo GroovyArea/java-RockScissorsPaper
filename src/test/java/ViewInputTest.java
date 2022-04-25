b56679e37e27d8ff51693450440eb1b92ecff505
NamYoung Kim <095201a@naver.com>
2022-04-25 13:56:20 +0900

    static Stream 메서드 추가와 MethodSource test 추가

--- src/test/java/ViewInputTest.java
+++ src/test/java/ViewInputTest.java
@@ -3,7 +3,11 @@ import org.junit.jupiter.api.Test;
 import Enum.*;
 import Exception.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.CoreMatchers.is;
@@ -11,7 +15,7 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class ViewInputTest {
 
-    @Test
+/*    @Test
     @DisplayName("입력한 정수가 enum 값으로 잘 바뀌었는지")
     void rspInput() throws RspException {
         Rsp rsp = Rsp.ROCK;
@@ -19,6 +23,21 @@ public class ViewInputTest {
         Rsp input = Rsp.of(1);
 
         assertThat(rsp == input).isTrue();
+    }*/
+
+    static Stream<Arguments> rsp(){
+        return Stream.of(
+          Arguments.arguments(1, Rsp.ROCK),
+          Arguments.arguments(2, Rsp.SCISSORS),
+          Arguments.arguments(3, Rsp.PAPER)
+        );
+    }
+
+    @ParameterizedTest(name = "입력 값 : {0}, 가위바위보 값 : {1}")
+    @MethodSource("rsp")
+    @DisplayName("입력 값에 맞는 가위바위보로 변경되는지")
+    void ofTest(Integer integer, Rsp rsp) throws RspException {
+        assertThat(Rsp.of(integer)).isEqualTo(rsp);
     }
 
 
